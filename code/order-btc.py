862f35d8aa68345e2b1afda4e8c76cdbf9bb966b
Marco <mkoepfli@hosttech.ch>
2022-05-02 14:45:22 +0200

    added report and improved env

--- code/order-btc.py
+++ code/order-btc.py
@@ -3,30 +3,29 @@ import pandas as pd
 import time, json
 from time import sleep
 import FTX
-import FTP
-import SendMail
+import Report
 
 c = FTX.FTX()
 
 CoinName = input("Coin name you like to watch: ")
 MakeTrade = input("Start Trade at percent move(int): ")
 Profit = input("Take Profit at this percent(int): ")
-Loss = input("Close Trade at this lose percent(int): ")
+Lose = input("Close Trade at this lose percent(int): ")
 
 while True:
     try:
         price_old = requests.get('https://ftx.com/api/markets/' + CoinName).json()
         print(price_old['result']['ask'])
     except Exception as e:
-        print(f'Error obtaining BTC old data: {e}')
+        print(f'Error fetching old coin data:: {e}')
 
     sleep(10)
 
     try:
-        price_new = requests.get('https://ftx.com/api/markets/BTC-PERP').json()
+        price_new = requests.get('https://ftx.com/api/markets/' + CoinName).json()
         print(price_new['result']['ask'])
     except Exception as e:
-        print(f'Error obtaining BTC new data: {e}')
+        print(f'Error fetching new coin data: {e}')
 
     percent = (((float(price_new['result']['ask']) - float(price_old['result']['ask'])) * 100) / float(
         price_old['result']['ask']))
@@ -37,15 +36,12 @@ while True:
 
     elif percent >= float(MakeTrade):
         try:
-            r = c.place_order("ETH/USD", "buy", 1800.0, 0.00006)
+            r = c.place_order("ETH/USD", "buy", 1.0, 0.006)
             print(r)
         except Exception as e:
             print(f'Error making order request: {e}')
 
         sleep(2)
-
-        FTP.FTP()
-        SendMail.SendMail()
 
         try:
             check = c.get_open_orders(r['id'])
@@ -54,6 +50,7 @@ while True:
 
         if check[0]['status'] == 'open':
             print('Order placed at {}'.format(pd.Timestamp.now()))
+            Report.create_report(CoinName, Lose, Profit)
             break
         else:
             print('Order was either filled or canceled at {}'.format(pd.Timestamp.now()))
